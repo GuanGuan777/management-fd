d8669b82d5faf80d8180e792c9f8ba5511261e27
smallcalia <ks34943@163.com>
2020-02-24 16:50:46 +0800

    :globe_with_meridians:修改配置文件

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about pagedfsdfsdfsdf</h1>
+    <h2>{{a}}</h2>
+    <div>
+      <div>
+        xxx
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+    export default{
+        name: 'about',
+        data(){
+          return{
+              a:1,
+          }
+        },
+        methods:{
+
+        },
+        beforeCreate(){
+
+        },
+        created () {
+
+        },
+        beforeMount(){
+
+        },
+        mounted(){
+
+        },
+        beforeUpdate(){
+
+        },
+        updated(){
+
+        },
+        beforeDestroy(){
+
+        },
+        destroyed(){
+
+        }
+    }
+
+</script>
+
+<style lang="sass">
+    .about{
+      color:#fff;
+      div{
+        background-color:#000;
+      }
+    }
+</style>
