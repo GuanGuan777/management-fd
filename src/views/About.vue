443aaa35b81aa1e9a6d64199150686f05c7c0056
smallcalia <ks34943@163.com>
2020-03-16 21:40:05 +0800

    :sparkles:修改部分文件

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,59 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about pagedfsdfsdfsdf</h1>
-    <h2>{{a}}</h2>
-    <div>
-      <div>
-        xxx
-      </div>
+    <div class="about">
+        <h1>This is an about pagedfsdfsdfsdf</h1>
+        <h2>{{ a }}</h2>
+        <div>
+            <div>
+                xxx
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-    export default{
-        name: 'about',
-        data(){
-          return{
-              a:1,
-          }
-        },
-        methods:{
-
-        },
-        beforeCreate(){
-
-        },
-        created () {
-
-        },
-        beforeMount(){
-
-        },
-        mounted(){
-
-        },
-        beforeUpdate(){
-
-        },
-        updated(){
-
-        },
-        beforeDestroy(){
-
-        },
-        destroyed(){
-
-        }
-    }
-
+export default {
+    name: "about",
+    data() {
+        return {
+            a: 1
+        };
+    },
+    methods: {},
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {}
+};
 </script>
 
-<style lang="sass">
-    .about{
-      color:#fff;
-      div{
-        background-color:#000;
-      }
+<style lang="scss">
+.about {
+    color: #fff;
+    div {
+        background-color: #000;
     }
+}
 </style>
